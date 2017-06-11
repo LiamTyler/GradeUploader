c0359989833b7947d61660f3431972b14428a630
LiamTyler <tyler.liam7@gmail.com>
2017-06-11 11:10:40 -0500

    feat(scoring): Spreadsheets are analyzed and a scores csv is created for each spreadsheet. Scoring rules are based off of the scoring.txt

--- file_downloader.py
+++ file_downloader.py
@@ -2,12 +2,19 @@ from __future__ import print_function
 import httplib2
 import os
 import io
+import csv
 
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from apiclient.http import MediaIoBaseDownload
 from oauth2client.file import Storage
+
+downloads_dirname = "spreadsheet_downloads"
+scores_dirname = "scores"
+home_dir = os.path.expanduser('./')
+downloads_dir = os.path.join(home_dir, downloads_dirname)
+scores_dir = os.path.join(home_dir, scores_dirname)
 
 try:
     import argparse
@@ -21,12 +28,17 @@ SCOPES = 'https://www.googleapis.com/auth/drive'
 CLIENT_SECRET_FILE = 'client_secrets.json'
 APPLICATION_NAME = 'Python'
 
-
-def get_credentials():
-    home_dir = os.path.expanduser('./')
+def makeDirs():
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
+    if not os.path.exists(downloads_dir):
+        os.makedirs(downloads_dir)
+    if not os.path.exists(scores_dir):
+        os.makedirs(scores_dir)
+    
+def get_credentials():
+    credential_dir = os.path.join(home_dir, '.credentials')
     credential_path = os.path.join(credential_dir,
                                    'drive-python-quickstart.json')
 
@@ -41,30 +53,82 @@ def get_credentials():
             credentials = tools.run(flow, store)
     return credentials
 
+def downloadFiles():
+    # ID of the 1113 folder
+    folder_id = '0B_LUTW8XEjWDNmE5UFhZSDhMN1U'
 
-# ID of the 1113 folder
-folder_id = '0B_LUTW8XEjWDNmE5UFhZSDhMN1U'
+    SRC_MIMETYPE = 'application/vnd.google-apps.spreadsheet'
+    DST_MIMETYPE = 'text/csv'
 
-SRC_MIMETYPE = 'application/vnd.google-apps.spreadsheet'
-DST_MIMETYPE = 'text/csv'
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('drive', 'v3', http=http)
 
-credentials = get_credentials()
-http = credentials.authorize(httplib2.Http())
-service = discovery.build('drive', 'v3', http=http)
+    files = service.files().list(q='mimeType="%s" and "%s" in parents' %
+                            (SRC_MIMETYPE, folder_id)).execute().get('files', [])
+    if files:
+        for f in files:
+            if 'Lab' in f['name']:
+                fn = '%s/%s.csv' % (downloads_dir, f['name'])
+                data = service.files().export(fileId=f['id'],
+                                        mimeType=DST_MIMETYPE).execute()
+                if data:
+                    with open(fn, 'wb') as fd:
+                        fd.write(data)
+        return True
+    else:
+        return False
 
-files = service.files().list(q='mimeType="%s" and "%s" in parents' %
-                        (SRC_MIMETYPE, folder_id)).execute().get('files', [])
-if files:
-    home_dir = os.path.expanduser('./')
-    files_dir = os.path.join(home_dir, 'spreadsheet_downloads')
-    if not os.path.exists(files_dir):
-        os.makedirs(files_dir)
+def gradeFile(fn, scoring):
+    # open csv reader to read the downloaded spreadsheets, and then open a
+    # csv write so that we can record the score of each student
+    fn_download = os.path.join(downloads_dir, fn)
+    input_csvfile = open(fn_download, newline='')
+    reader = csv.reader(input_csvfile, delimiter=' ', quotechar='|')
+    fn_scores = os.path.join(scores_dir, fn)
+    output_csvfile = open(fn_scores, 'w+', newline='')
+    writer = csv.writer(output_csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
-    for f in files:
-        if 'Lab' in f['name']:
-            fn = 'spreadsheet_downloads/%s.csv' % f['name']
-            data = service.files().export(fileId=f['id'],
-                                    mimeType=DST_MIMETYPE).execute()
-            if data:
-                with open(fn, 'wb') as fd:
-                    fd.write(data)
+    header = reader.__next__()[0].split(',')
+    header = header[:2] + ['score']
+    writer.writerow(header)
+    for row in reader:
+        row = row[0].split(',')
+        name = row[:2]
+        row = row[2:]
+        score = 0
+        for i in range(len(row)):
+            if row[i] == 'X':
+                score += scoring[i]
+        row = name + [score]
+        writer.writerow(row)
+    input_csvfile.close()
+    output_csvfile.close()
+
+def main():
+    makeDirs()
+    #if downloadFiles():
+    dFiles = [f for f in os.listdir(downloads_dir) if
+              os.path.isfile(os.path.join(downloads_dir, f))]
+    # Read in scoring distributions
+    f = open("scoring.txt")
+    r = f.readlines()
+    f.close()
+    scores = {}
+    for row in r:
+        colon = row.find(':')
+        name = row[:colon]
+        s = row[colon + 1:]
+        s = s.strip()
+        s = s.split()
+        s = [int(x) for x in s]
+        scores[name] = s
+
+    print(scores)
+    for f in dFiles:
+        name = f[:f.find('.')]
+        gradeFile(f, scores[name])
+
+
+if __name__=='__main__':
+    main()
